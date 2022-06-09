bd1044689333fc30f98b24432d80862aca0b62cd
Or Linzer <orlinzer@gmail.com>
2022-06-09 17:35:35 +0300

    add post with a button

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ console.log("masheoo");
 
 function newPost() {
   console.log("masheoo");
+
+  let postText = document.getElementById('input');
+
+  let post = document.createElement('div');
+  post.innerHTML = postText.value;
+  post.className = 'post';
+  document.body.appendChild(post);
+
 }
